8401f164239a1fb7fd6e88fb8b37960f5f325ba9
Yavor Spassov <ygspasov@gmail.com>
2020-04-18 15:46:34 +0300

    Adding Vuetify

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Solutions
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Post your solutions to the problems you have solved.
+        </p>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+   
+  }
+</script>
