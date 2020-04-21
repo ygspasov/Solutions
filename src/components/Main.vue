b420af337829e49d170549300332d864e31ff801
Yavor Spassov <ygspasov@gmail.com>
2020-04-21 21:39:03 +0300

    Moving the signup method into the auth  mixin

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <v-row class="text-center">
-
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           Solutions
@@ -11,16 +10,15 @@
           Post your solutions to the problems you have solved.
         </p>
       </v-col>
-
     </v-row>
-    <v-row><Solutions/></v-row>
+    <v-row><Solutions /></v-row>
   </v-container>
 </template>
 
 <script>
-import Solutions from './Solutions'
-  export default {
-   name: 'Main',
-   components: {Solutions}
-  }
+import Solutions from "./Solutions";
+export default {
+  name: "Main",
+  components: { Solutions }
+};
 </script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,83 +1,51 @@
 <template>
-  <v-form v-model="valid" >
-    <v-container >
-        
-          <v-row v-if="!successMsg"
-          cols="12"
-          md="4"
-          style= "max-width:300px;" class="mx-auto"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail" 
-            required
-          ></v-text-field>
-        </v-row>
-        <v-row v-if="!successMsg"
-          cols="12"
-          md="4"
-          style= "max-width:300px;" class="mx-auto"
-        >
-          <v-text-field
-            label="Password"
-            v-model="password"
-            :rules="passwordRules"
-            required
-          ></v-text-field>
-        </v-row>
-        
-        <v-row><p v-if="successMsg" class="success--text mt-2 mx-auto">{{successMsg}}</p></v-row>
-        <v-row><p v-if="errMsg" class="error--text mt-2 mx-auto">{{errMsg}}</p></v-row>
-        <v-row v-if="!successMsg"><v-btn
-        :disabled="!valid"
-        color="success"
-        class="mx-auto mt-2"
-        @click="signup"
-      >
-        Sign up
-      </v-btn></v-row>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row v-if="!successMsg" cols="12" md="4" style="max-width:300px;" class="mx-auto">
+        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+      </v-row>
+      <v-row v-if="!successMsg" cols="12" md="4" style="max-width:300px;" class="mx-auto">
+        <v-text-field label="Password" v-model="password" :rules="passwordRules" required></v-text-field>
+      </v-row>
 
+      <v-row>
+        <p v-if="successMsg" class="success--text mt-2 mx-auto">{{ successMsg }}</p>
+      </v-row>
+      <v-row>
+        <p v-if="errMsg" class="error--text mt-2 mx-auto">{{ errMsg }}</p>
+      </v-row>
+      <v-row v-if="!successMsg">
+        <v-btn :disabled="!valid" color="success" class="mx-auto mt-2" @click="signup">Sign up</v-btn>
+      </v-row>
     </v-container>
   </v-form>
 </template>
 
 <script>
-import db from '../db/db'
-  export default {
-    data: () => ({
-      valid: false,
-      errMsg:'',
-      successMsg:"",
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
-      ],
-      password:'',
-      passwordRules: [
-        v => !!v || 'Password is required',
-        v => /^[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(v) || 'Password must be between 6 and 16 characters',
-      ],
-    }),
-    methods: {
-      signup(){
-        this.errMsg
-        db.auth().createUserWithEmailAndPassword(this.email, this.password).then(()=>{
-          this.successMsg="You have successfully signed up!"
-        }).catch((error)=> {
-    var errorCode = error.code;
-    console.log(errorCode)
-    this.errMsg= error.message;
-    console.log("Error message: "+this.errMsg)
-  });
-  this.email='',
-  this.password=''
-      }
-    }
-  }
+import authMixin from "../auth/auth";
+export default {
+  data: () => ({
+    valid: false,
+    errMsg: "",
+    successMsg: "",
+    nameRules: [
+      v => !!v || "Name is required",
+      v => v.length <= 10 || "Name must be less than 10 characters"
+    ],
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+/.test(v) || "E-mail must be valid"
+    ],
+    password: "",
+    passwordRules: [
+      v => !!v || "Password is required",
+      v =>
+        /^[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(v) ||
+        "Password must be between 6 and 16 characters"
+    ]
+  }),
+  mixins: [authMixin],
+  methods: {}
+};
 </script>
